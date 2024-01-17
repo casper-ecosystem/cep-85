fc215b27a93f5cf34d195f29f8603331654cff7e
gRoussac <gRoussac@users.noreply.github.com>
2024-01-17 11:58:27 +0100

    Missing events

--- client-js/src/events.ts
+++ client-js/src/events.ts
@@ -1,4 +1,4 @@
-import { CLKey, CLU256, CLValue } from 'casper-js-sdk';
+import { CLBool, CLKey, CLList, CLMap, CLOption, CLString, CLU256, CLValue } from 'casper-js-sdk';
 
 export type Event<E extends Record<string, CLValue>> = {
   name: string;
@@ -19,11 +19,27 @@ export type CEP85EventWithDeployInfo = WithDeployInfo<CEP85Event>;
 export type CEP85Event = Event<
   | Mint
   | Burn
+  | ApprovalForAll
+  | TransferSingle
+  | TransferBatch
+  | Uri
+  | SetTotalSupply
+  | ChangeSecurity
+  | SetModalities
+  | Upgrade
 >;
 
 export type EventsMap = {
   Mint: Event<Mint>;
   Burn: Event<Burn>;
+  ApprovalForAll: Event<ApprovalForAll>;
+  TransferSingle: Event<TransferSingle>;
+  TransferBatch: Event<TransferBatch>;
+  Uri: Event<Uri>;
+  SetTotalSupply: Event<SetTotalSupply>;
+  ChangeSecurity: Event<ChangeSecurity>;
+  SetModalities: Event<SetModalities>;
+  Upgrade: Event<Upgrade>;
 };
 
 export type Mint = {
@@ -37,3 +53,44 @@ export type Burn = {
   owner: CLKey;
   amount: CLU256;
 };
+
+export type ApprovalForAll = {
+  owner: CLKey,
+  operator: CLKey,
+  approved: CLBool,
+};
+
+export type TransferSingle = {
+  operator: CLKey,
+  from: CLKey,
+  to: CLKey,
+  id: CLU256,
+  value: CLU256,
+};
+
+export type TransferBatch = {
+  operator: CLKey,
+  from: CLKey,
+  to: CLKey,
+  ids: CLList<CLU256>,
+  values: CLList<CLU256>,
+};
+
+export type Uri = {
+  value: CLString,
+  id: CLOption<CLU256>,
+};
+
+export type SetTotalSupply = {
+  id: CLU256,
+  total_supply: CLU256,
+};
+
+export type ChangeSecurity = {
+  admin: CLKey,
+  sec_change_map: CLMap<CLKey, CLValue>,
+};
+
+export type SetModalities = Record<string, never>;
+
+export type Upgrade = Record<string, never>;
