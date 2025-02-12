465887d01046f03b1ac634f3287a6cbb6b0faed7
gRoussac <gRoussac@users.noreply.github.com>
2025-02-12 15:12:42 +0100

    Cleanup utils

--- contracts/contract/src/utils.rs
+++ contracts/contract/src/utils.rs
@@ -3,113 +3,126 @@ use crate::{
     constants::{ARG_TRANSFER_FILTER_CONTRACT, ARG_TRANSFER_FILTER_METHOD},
     error::Cep85Error,
 };
-#[cfg(feature = "contract-support")]
-use alloc::{borrow::ToOwned, vec, vec::Vec};
 use alloc::{format, string::String};
+#[cfg(feature = "contract-support")]
+use alloc::{vec, vec::Vec};
 #[cfg(feature = "contract-support")]
 use casper_contract::{
     contract_api::{
-        self,
-        runtime::{self, get_protocol_version},
-        storage,
+        alloc_bytes,
+        runtime::{
+            blake2b, get_immediate_caller as casper_get_immediate_caller, get_key,
+            get_protocol_version, revert,
+        },
+        storage::{dictionary_get, dictionary_put, read},
     },
-    ext_ffi,
+    ext_ffi::{
+        casper_get_key, casper_get_named_arg, casper_get_named_arg_size, casper_read_host_buffer,
+        casper_read_value,
+    },
     unwrap_or_revert::UnwrapOrRevert,
 };
 use casper_types::U256;
 #[cfg(feature = "contract-support")]
 use casper_types::{
-    api_error,
-    bytesrepr::{self, FromBytes, ToBytes},
-    contracts::ContractVersionKey,
-    system::CallStackElement,
-    AddressableEntityHash, ApiError, CLTyped, EntityAddr, Key, URef,
+    account::AccountHash,
+    api_error::result_from,
+    bytesrepr::{deserialize, FromBytes, ToBytes},
+    contracts::{ContractHash, ContractPackageHash, ContractVersionKey},
+    AddressableEntityHash, ApiError, CLTyped, EntityAddr, Key, PackageHash, URef,
 };
 #[cfg(feature = "contract-support")]
 use core::{convert::TryInto, mem::MaybeUninit};
+#[cfg(feature = "contract-support")]
+use hex::encode;
 
 #[cfg(feature = "contract-support")]
-// TODO CHECK *runtime::get_call_stack()
-/// ! TODO GR
-fn read_host_buffer(size: usize) -> Result<Vec<u8>, ApiError> {
-    let mut dest: Vec<u8> = if size == 0 {
-        Vec::new()
-    } else {
-        let bytes_non_null_ptr = contract_api::alloc_bytes(size);
-        unsafe { Vec::from_raw_parts(bytes_non_null_ptr.as_ptr(), size, size) }
-    };
-    read_host_buffer_into(&mut dest)?;
-    Ok(dest)
-}
-
-#[cfg(feature = "contract-support")]
-// TODO CHECK *runtime::get_call_stack()
-/// ! TODO GR
-fn read_host_buffer_into(dest: &mut [u8]) -> Result<usize, ApiError> {
-    let mut bytes_written = MaybeUninit::uninit();
-    let ret = unsafe {
-        ext_ffi::casper_read_host_buffer(dest.as_mut_ptr(), dest.len(), bytes_written.as_mut_ptr())
-    };
-    // NOTE: When rewriting below expression as `result_from(ret).map(|_| unsafe { ... })`, and the
-    // caller ignores the return value, execution of the contract becomes unstable and ultimately
-    // leads to `Unreachable` error.
-    api_error::result_from(ret)?;
-    Ok(unsafe { bytes_written.assume_init() })
-}
-
-#[cfg(feature = "contract-support")]
-// TODO CHECK *runtime::get_call_stack()
-/// ! TODO GR
-pub fn get_call_stack() -> Vec<CallStackElement> {
-    let (call_stack_len, result_size) = {
-        let mut call_stack_len: usize = 0;
-        let mut result_size: usize = 0;
-        let ret = unsafe {
-            #[allow(deprecated)]
-            ext_ffi::casper_load_call_stack(
-                &mut call_stack_len as *mut usize,
-                &mut result_size as *mut usize,
-            )
-        };
-        api_error::result_from(ret).unwrap_or_revert();
-        (call_stack_len, result_size)
-    };
-    if call_stack_len == 0 {
-        return Vec::new();
-    }
-    let bytes = read_host_buffer(result_size).unwrap_or_revert();
-    bytesrepr::deserialize(bytes).unwrap_or_revert()
-}
-
-#[cfg(feature = "contract-support")]
-// CHECK *runtime::get_call_stack() // get_immediate_caller() CallerInfo into Key (Caller ?)
-/// ! TODO GR
 pub fn get_immediate_caller() -> (Key, Option<Key>) {
-    match *get_call_stack()
-        .iter()
-        .nth_back(1)
-        .to_owned()
-        .unwrap_or_revert()
-    {
-        CallStackElement::Session { account_hash } => (
-            Key::AddressableEntity(EntityAddr::Account(account_hash.value())),
-            None,
-        ),
-        CallStackElement::StoredSession {
-            account_hash: _, // Caller is contract
-            contract_package_hash,
-            contract_hash,
-        } => (
-            Key::AddressableEntity(EntityAddr::SmartContract(contract_hash.value())),
-            Some(Key::SmartContract(contract_package_hash.value())),
-        ),
-        CallStackElement::StoredContract {
-            contract_package_hash,
-            contract_hash,
-        } => (
-            Key::AddressableEntity(EntityAddr::SmartContract(contract_hash.value())),
-            Some(Key::SmartContract(contract_package_hash.value())),
-        ),
+    const ACCOUNT: u8 = 0;
+    const PACKAGE: u8 = 1;
+    const CONTRACT_PACKAGE: u8 = 2;
+    const ENTITY: u8 = 3;
+    const CONTRACT: u8 = 4;
+
+    let caller_info = casper_get_immediate_caller().unwrap_or_revert();
+
+    match caller_info.kind() {
+        ACCOUNT => {
+            let account_hash = caller_info
+                .get_field_by_index(ACCOUNT)
+                .unwrap()
+                .to_t::<Option<AccountHash>>()
+                .unwrap_or_revert()
+                .unwrap_or_revert_with(Cep85Error::UnexpectedKeyVariant);
+            (
+                Key::AddressableEntity(EntityAddr::Account(account_hash.value())),
+                None,
+            )
+        }
+        PACKAGE => {
+            let package_hash = caller_info
+                .get_field_by_index(PACKAGE)
+                .unwrap()
+                .to_t::<Option<PackageHash>>()
+                .unwrap_or_revert()
+                .unwrap_or_revert_with(Cep85Error::UnexpectedKeyVariant);
+            let contract_hash = caller_info
+                .get_field_by_index(CONTRACT)
+                .unwrap()
+                .to_t::<Option<ContractHash>>()
+                .unwrap_or_revert()
+                .unwrap_or_revert_with(Cep85Error::UnexpectedKeyVariant);
+            (
+                Key::contract_entity_key(contract_hash.into()),
+                Some(Key::SmartContract(package_hash.value())),
+            )
+        }
+        CONTRACT_PACKAGE => {
+            let contract_package_hash = caller_info
+                .get_field_by_index(CONTRACT_PACKAGE)
+                .unwrap()
+                .to_t::<Option<ContractPackageHash>>()
+                .unwrap_or_revert()
+                .unwrap_or_revert_with(Cep85Error::UnexpectedKeyVariant);
+            let contract_hash = caller_info
+                .get_field_by_index(CONTRACT)
+                .unwrap()
+                .to_t::<Option<ContractHash>>()
+                .unwrap_or_revert()
+                .unwrap_or_revert_with(Cep85Error::UnexpectedKeyVariant);
+            (
+                Key::contract_entity_key(contract_hash.into()),
+                Some(Key::SmartContract(contract_package_hash.value())),
+            )
+        }
+        ENTITY => {
+            let entity_addr = caller_info
+                .get_field_by_index(ENTITY)
+                .unwrap()
+                .to_t::<Option<EntityAddr>>()
+                .unwrap_or_revert()
+                .unwrap_or_revert_with(Cep85Error::UnexpectedKeyVariant);
+            (Key::from(entity_addr), None)
+        }
+        CONTRACT => {
+            let contract_hash = caller_info
+                .get_field_by_index(CONTRACT)
+                .unwrap()
+                .to_t::<Option<ContractHash>>()
+                .unwrap_or_revert()
+                .unwrap_or_revert_with(Cep85Error::UnexpectedKeyVariant);
+            let contract_package_hash = caller_info
+                .get_field_by_index(CONTRACT_PACKAGE)
+                .unwrap()
+                .to_t::<Option<ContractPackageHash>>()
+                .unwrap_or_revert()
+                .unwrap_or_revert_with(Cep85Error::UnexpectedKeyVariant);
+            (
+                Key::contract_entity_key(contract_hash.into()),
+                Some(Key::SmartContract(contract_package_hash.value())),
+            )
+        }
+        _ => revert(Cep85Error::UnexpectedKeyVariant),
     }
 }
 
@@ -125,7 +138,7 @@ where
     T: FromBytes + CLTyped,
 {
     let uref = get_uref(name);
-    storage::read(uref)
+    read(uref)
         .unwrap_or_revert_with(Cep85Error::UrefNotFound)
         .unwrap_or_revert_with(Cep85Error::FailedToReadFromStorage)
 }
@@ -139,9 +152,9 @@ pub fn get_named_arg_with_user_errors<T: FromBytes>(
     let arg_size = get_named_arg_size(name).ok_or(missing)?;
     let arg_bytes = if arg_size > 0 {
         let res = {
-            let data_non_null_ptr = contract_api::alloc_bytes(arg_size);
+            let data_non_null_ptr = alloc_bytes(arg_size);
             let ret = unsafe {
-                ext_ffi::casper_get_named_arg(
+                casper_get_named_arg(
                     name.as_bytes().as_ptr(),
                     name.len(),
                     data_non_null_ptr.as_ptr(),
@@ -150,7 +163,7 @@ pub fn get_named_arg_with_user_errors<T: FromBytes>(
             };
             let data =
                 unsafe { Vec::from_raw_parts(data_non_null_ptr.as_ptr(), arg_size, arg_size) };
-            api_error::result_from(ret).map(|_| data)
+            result_from(ret).map(|_| data)
         };
         // Assumed to be safe as `get_named_arg_size` checks the argument already
         res.unwrap_or_revert_with(Cep85Error::FailedToGetArgBytes)
@@ -159,7 +172,7 @@ pub fn get_named_arg_with_user_errors<T: FromBytes>(
         Vec::new()
     };
 
-    bytesrepr::deserialize(arg_bytes).map_err(|_| invalid)
+    deserialize(arg_bytes).map_err(|_| invalid)
 }
 
 #[cfg(feature = "contract-support")]
@@ -170,7 +183,7 @@ pub fn get_optional_named_arg_with_user_errors<T: FromBytes>(
     match get_named_arg_with_user_errors::<T>(name, Cep85Error::Phantom, invalid) {
         Ok(val) => Some(val),
         Err(Cep85Error::Phantom) => None,
-        Err(e) => runtime::revert(e),
+        Err(e) => revert(e),
     }
 }
 
@@ -198,8 +211,8 @@ pub fn make_dictionary_item_key<T: CLTyped + ToBytes, V: CLTyped + ToBytes>(
 
     bytes_a.append(&mut bytes_b);
 
-    let bytes = runtime::blake2b(bytes_a);
-    hex::encode(bytes)
+    let bytes = blake2b(bytes_a);
+    encode(bytes)
 }
 
 #[cfg(feature = "contract-support")]
@@ -213,9 +226,9 @@ pub fn get_dictionary_value_from_key<T: CLTyped + FromBytes>(
         Cep85Error::InvalidStorageUref,
     );
 
-    match storage::dictionary_get::<T>(seed_uref, key) {
+    match dictionary_get::<T>(seed_uref, key) {
         Ok(maybe_value) => maybe_value,
-        Err(error) => runtime::revert(error),
+        Err(error) => revert(error),
     }
 }
 
@@ -230,7 +243,7 @@ pub fn set_dictionary_value_for_key<T: CLTyped + ToBytes + Copy>(
         Cep85Error::MissingStorageUref,
         Cep85Error::InvalidStorageUref,
     );
-    storage::dictionary_put::<T>(seed_uref, key, *value)
+    dictionary_put::<T>(seed_uref, key, *value)
 }
 
 #[cfg(feature = "contract-support")]
@@ -257,7 +270,7 @@ pub fn replace_token_id_in_uri(raw_uri: &str, id: &U256) -> String {
 
 #[cfg(feature = "contract-support")]
 fn get_uref(name: &str) -> URef {
-    let key = runtime::get_key(name)
+    let key = get_key(name)
         .ok_or(ApiError::MissingKey)
         .unwrap_or_revert_with(Cep85Error::FailedToGetKey);
     key.try_into()
@@ -277,7 +290,7 @@ fn get_key_with_user_errors(name: &str, missing: Cep85Error, invalid: Cep85Error
     let mut key_bytes = vec![0u8; Key::max_serialized_length()];
     let mut total_bytes: usize = 0;
     let ret = unsafe {
-        ext_ffi::casper_get_key(
+        casper_get_key(
             name_ptr,
             name_size,
             key_bytes.as_mut_ptr(),
@@ -285,14 +298,14 @@ fn get_key_with_user_errors(name: &str, missing: Cep85Error, invalid: Cep85Error
             &mut total_bytes as *mut usize,
         )
     };
-    match api_error::result_from(ret) {
+    match result_from(ret) {
         Ok(_) => {}
-        Err(ApiError::MissingKey) => runtime::revert(missing),
-        Err(e) => runtime::revert(e),
+        Err(ApiError::MissingKey) => revert(missing),
+        Err(e) => revert(e),
     }
     key_bytes.truncate(total_bytes);
 
-    bytesrepr::deserialize(key_bytes).unwrap_or_revert_with(invalid)
+    deserialize(key_bytes).unwrap_or_revert_with(invalid)
 }
 
 #[cfg(feature = "contract-support")]
@@ -304,19 +317,40 @@ fn read_with_user_errors<T: CLTyped + FromBytes>(
     let key: Key = uref.into();
     let (key_ptr, key_size, _bytes) = to_ptr(key);
 
+    // Get the size of the value
     let value_size = {
         let mut value_size = MaybeUninit::uninit();
-        let ret = unsafe { ext_ffi::casper_read_value(key_ptr, key_size, value_size.as_mut_ptr()) };
-        match api_error::result_from(ret) {
+        let ret = unsafe { casper_read_value(key_ptr, key_size, value_size.as_mut_ptr()) };
+        match result_from(ret) {
             Ok(_) => unsafe { value_size.assume_init() },
-            Err(ApiError::ValueNotFound) => runtime::revert(missing),
-            Err(e) => runtime::revert(e),
+            Err(ApiError::ValueNotFound) => revert(missing),
+            Err(e) => revert(e),
         }
     };
 
-    let value_bytes = read_host_buffer(value_size).unwrap_or_revert();
+    // Allocate a buffer to store the value
+    let mut buffer = vec![0u8; value_size];
+    let mut bytes_written = 0usize;
 
-    bytesrepr::deserialize(value_bytes).unwrap_or_revert_with(invalid)
+    let ret = unsafe {
+        casper_read_host_buffer(
+            buffer.as_mut_ptr(),
+            value_size,
+            &mut bytes_written as *mut usize,
+        )
+    };
+
+    // Check for errors
+    match result_from(ret) {
+        Ok(_) => {}
+        Err(e) => revert(e),
+    }
+
+    if bytes_written != value_size {
+        revert(ApiError::UnexpectedKeyVariant);
+    }
+
+    deserialize(buffer).unwrap_or_revert_with(invalid)
 }
 
 #[cfg(feature = "contract-support")]
@@ -331,15 +365,15 @@ fn to_ptr<T: ToBytes>(t: T) -> (*const u8, usize, Vec<u8>) {
 fn get_named_arg_size(name: &str) -> Option<usize> {
     let mut arg_size: usize = 0;
     let ret = unsafe {
-        ext_ffi::casper_get_named_arg_size(
+        casper_get_named_arg_size(
             name.as_bytes().as_ptr(),
             name.len(),
             &mut arg_size as *mut usize,
         )
     };
-    match api_error::result_from(ret) {
+    match result_from(ret) {
         Ok(_) => Some(arg_size),
         Err(ApiError::MissingArgument) => None,
-        Err(e) => runtime::revert(e),
+        Err(e) => revert(e),
     }
 }
